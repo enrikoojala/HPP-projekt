b2a99f7004e8efe149e27ee11e54231d839276c3
Enriko Ojala <eojala22@tlu.ee>
2019-11-05 23:00:08 +0200

    QR kood, JavaScript, html muudatused

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@
 
 // insert script here
 // https://www.w3schools.com/js/js_window_location.asp
+
+//QR-code/Ruutkoodi genereerimine
+//Siin saab saab 
+
+let hostname = window.location.hostname;
+
+//test muutujad
+let reino_lp_url = "www.ristissaar.ee/hpp/rollid/linnaplaneerija/";
+
+//let lp_url = hostname + ":5500/rollid/linnaplaneerija/";
+let i_url = hostname + ":5500/rollid/investor/";
+let sn_url = hostname + ":5500/rollid/sotsiaalnounik/";
+let lkb_url = hostname + ":5500/rollid/looduskaitsebioloog/";
+let kva_url = hostname + ":5500/rollid/kinnisvaraarendaja/";
+let kd_url = hostname + ":5500/rollid/koolidirektor/";
+let ake_url = hostname + ":5500/rollid/aktiivne_linnaelanik/";
+
+let urls = [i_url, sn_url, lkb_url, kva_url, kd_url, ake_url];
+
+//Peab olema on load muidu ei tööta.
+$(window).on('load',function(){
+
+  urls.forEach(makeQRCode);
+
+  function makeQRCode(item, index) {
+    let qrcode = new QRCode(document.getElementById("qrcode_"+index), {
+      text: item,
+      width: 200,
+      height: 200
+    });
+  }
+});
