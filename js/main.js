ef1dab419e683d8fc1755e2f8d5c11215a30cb9f
reino7 <reino7@tlu.ee>
2019-11-06 07:23:29 +0200

    lokaalses testimiseks rollide link dünaamiliseks

--- js/main.js
+++ js/main.js
@@ -12,17 +12,22 @@
 //Siin saab saab 
 
 let hostname = window.location.hostname;
+let port = window.location.port;
+
+if (port = 5500) {
+  port = ':' + port;
+}
 
 //test muutujad
 let reino_lp_url = "www.ristissaar.ee/hpp/rollid/linnaplaneerija/";
 
 //let lp_url = hostname + ":5500/rollid/linnaplaneerija/";
-let i_url = hostname + ":5500/rollid/investor/";
-let sn_url = hostname + ":5500/rollid/sotsiaalnounik/";
-let lkb_url = hostname + ":5500/rollid/looduskaitsebioloog/";
-let kva_url = hostname + ":5500/rollid/kinnisvaraarendaja/";
-let kd_url = hostname + ":5500/rollid/koolidirektor/";
-let ake_url = hostname + ":5500/rollid/aktiivne_linnaelanik/";
+let i_url = hostname + port + "/rollid/investor/";
+let sn_url = hostname + port + "/rollid/sotsiaalnounik/";
+let lkb_url = hostname + port + "/rollid/looduskaitsebioloog/";
+let kva_url = hostname + port + "/rollid/kinnisvaraarendaja/";
+let kd_url = hostname + port + ":/rollid/koolidirektor/";
+let ake_url = hostname + port + "/rollid/aktiivne_linnaelanik/";
 
 let urls = [i_url, sn_url, lkb_url, kva_url, kd_url, ake_url];
 
