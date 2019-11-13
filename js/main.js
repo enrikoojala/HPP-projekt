a831129cef124871fb799150fc0e74613eaf0312
Enriko Ojala <eojala22@tlu.ee>
2019-11-13 23:08:02 +0200

    Andmete salvestamine ja kÄttesaamine, func töötavad ainult omal lehtedel

--- js/main.js
+++ js/main.js
@@ -1,60 +1,87 @@
 $(window).on('load',function(){
-    // When the HTML Body element will load it will display introductionModal
-    // $('#introductionModal').modal('show');
+  // Get hostname, port and set urls.
+
+  //host nimi koos pordiga
+  let host = location.host;
+  //Hetke asukoht URL'is.
+  let x = location.href;
+  let cname_popUrl = "http://" + host + "/cityname_and_population.html";
+  let gameboardURL = "http://" + host + "/gameboard.html";
+
+  // When the HTML Body element will load it will display introductionModal
+  // $('#introductionModal').modal('show');
 
   //Hetkel andmete timer. Tuleb teha funktsioonides muudatusi kui tahame teha dünaamilisemaks teiste lehtede jaoks ka VB.
-  function countdown(seconds) {
-    seconds = parseInt(sessionStorage.getItem("seconds")) || seconds;
-    //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab. 
-    //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
-    //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
+  //kui urlid kattuvad, siis läheb kood tööle, cityname_and_population.html lehel.
+  if(cname_popUrl == x) {
+    
+    function countdown(seconds) {
+      seconds = parseInt(sessionStorage.getItem("seconds")) || seconds;
+      //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab. 
+      //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
+      //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
 
-    function tick() {
-      seconds--;
-      //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi. 
-      //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
-      sessionStorage.setItem("seconds", seconds)
-      var counter = document.getElementById("timer_1");
-      var current_minutes = parseInt(seconds/60);
-      var current_seconds = seconds % 60;
-      
-      if(current_minutes < 3){
-        //Bootstrap roheline värv
-        counter.style.color = "#28a745";
+      function tick() {
+        seconds--;
+        //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi. 
+        //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
+        sessionStorage.setItem("seconds", seconds)
+        let counter = document.getElementById("timer_1");
+        let current_minutes = parseInt(seconds/60);
+        let current_seconds = seconds % 60;
+        
+        if(current_minutes < 3){
+          //Bootstrap roheline värv
+          counter.style.color = "#28a745";
+        }
+        if(current_minutes < 2){
+          //Bootstrap kollane/oranz värv
+          counter.style.color = "#ffc107";
+        }
+        if(current_minutes < 1){
+          //Bootstrap punane värv
+          counter.style.color = "#dc3545";
+        }
+
+        //Andmete välja näitamine html kujul
+        counter.innerHTML = current_minutes + ":" + (current_seconds < 10 ? "0" : "") + current_seconds;
+        if( seconds > 0 ) {
+          setTimeout(tick, 1000);
+        }
+        if(seconds <= 0){
+          $("#andmedTimerModal").modal("show");
+        }
       }
-      if(current_minutes < 2){
-        //Bootstrap kollane/oranz värv
-        counter.style.color = "#ffc107";
-      }
-      if(current_minutes < 1){
-        //Bootstrap punane värv
-        counter.style.color = "#dc3545";
-      }
-      //Andmete välja näitamine html kujul
-      counter.innerHTML = current_minutes + ":" + (current_seconds < 10 ? "0" : "") + current_seconds;
-      if( seconds > 0 ) {
-        setTimeout(tick, 1000);
-      }
-      if(seconds <= 0){
-        $("#andmedTimerModal").modal("show");
-      }
+      tick();
     }
-    tick();
+    countdown(180);
+  
+    
+    document.getElementById("submit").addEventListener("click", saveData);
+    
+    //Linnanime ja rahvaarvu salvestamine
+    function saveData(){
+      let cityName = document.getElementById("cityName").value;
+      let population = document.getElementById("population").value;
+
+      sessionStorage.setItem("cityName", cityName);
+      sessionStorage.setItem("population", population);
+
+      return true;
+    }
+
   }
-  countdown(180);
+  
+  //Kood läheb tööle gameboard.html lehel.
+  if(gameboardURL == x){
 
-// Get Cityname and Population
-// var cityName = document.getElementById("cityName").value;
-// var population = document.getElementById("population").value;
+    //Linnanime ja rahvaarvu kättesaamine.
+    let cityNameShow = sessionStorage.getItem("cityName");
+    let populationShow = sessionStorage.getItem("population");
 
+    document.getElementById("cityName").innerHTML = cityNameShow;
+    document.getElementById("population").innerHTML = populationShow;
 
-//QR-code/Ruutkoodi genereerimine
-
-// Get Hostname and port
-// let hostname = window.location.hostname;
-// let port = window.location.port;
-
-// if (port = 5500) {
-//   port = ':' + port;
-// }
+    document.getElementsByTagName("Title")[0].text = cityNameShow + " Elukeskkonna planeerimine";
+  }
 });
