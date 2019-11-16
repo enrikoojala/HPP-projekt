2f06c2342e43df3559c45455cc20764bb0f4125c
enrikoojala <eojala22@tlu.ee>
2019-11-16 11:06:27 +0200

    Mänguplaanile taimer

--- js/main.js
+++ js/main.js
@@ -86,6 +86,54 @@ $(window).on('load', function() {
 
     document.getElementsByTagName('Title')[0].text =
       cityNameShow + ' Elukeskkonna planeerimine';
+
+      function countdown(seconds) {
+        seconds = parseInt(sessionStorage.getItem('Timer 2')) || seconds;
+        //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab.
+        //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
+        //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
+  
+        function tick() {
+          seconds--;
+          //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi.
+          //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
+          sessionStorage.setItem('Timer 2', seconds);
+          let timer_2 = document.getElementById('timer_2');
+          let current_minutes = parseInt(seconds / 60);
+          let current_seconds = seconds % 60;
+  
+          // if (current_minutes < 3) {
+          //   //Bootstrap roheline värv
+          //   timer_2.style.color = '#28a745';
+          // }
+          // if (current_minutes < 2) {
+          //   //Bootstrap kollane/oranz värv
+          //   timer_2.style.color = '#ffc107';
+          // }
+          // if (current_minutes < 1) {
+          //   //Bootstrap punane värv
+          //   timer_2.style.color = '#dc3545';
+          // }
+  
+          //Andmete välja näitamine html kujul
+          timer_2.innerHTML =
+            current_minutes +
+            ':' +
+            (current_seconds < 10 ? '0' : '') +
+            current_seconds;
+          if (seconds > 0) {
+            setTimeout(tick, 1000);
+          }
+          if (seconds == 10) {
+            $('#halfTimeModal').modal('show');
+          }
+          if (seconds <= 0) {
+            $('#gameOverModal').modal('show');
+          }
+        }
+        tick();
+      }
+      countdown(20);
   }
 
   // //Hetke asukoht URL'is.
