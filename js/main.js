0686fec9f438ff73518306d17179bc6de1c6b833
reino7 <reino7@tlu.ee>
2019-11-21 09:47:58 +0200

    improved cityname_population_cityplanner ui

--- js/main.js
+++ js/main.js
@@ -90,7 +90,6 @@ $(window).on('load', function() {
 
   if (fileName == 'cityname_population_cityplanner.html') {
     document.getElementById('rollidLinnaplaneerijaID').innerHTML = linnaplaneerijaUrl;
-
     let urls = [linnaplaneerijaUrl];
 
     urls.forEach(makeQRCode);
@@ -102,6 +101,51 @@ $(window).on('load', function() {
         height: 175
       });
     }
+
+    function countdown(seconds) {
+      seconds = parseInt(sessionStorage.getItem('seconds')) || seconds;
+      //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab.
+      //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
+      //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
+
+      function tick() {
+        seconds--;
+        //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi.
+        //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
+        sessionStorage.setItem('seconds', seconds);
+        let counter = document.getElementById('timer_1');
+        let current_minutes = parseInt(seconds / 60);
+        let current_seconds = seconds % 60;
+
+        // if (current_minutes < 3) {
+        //   //Bootstrap roheline värv
+        //   counter.style.color = '#28a745';
+        // }
+        // if (current_minutes < 2) {
+        //   //Bootstrap kollane/oranz värv
+        //   counter.style.color = '#ffc107';
+        // }
+        // if (current_minutes < 1) {
+        //   //Bootstrap punane värv
+        //   counter.style.color = '#dc3545';
+        // }
+
+        //Andmete välja näitamine html kujul
+        counter.innerHTML =
+          current_minutes +
+          ':' +
+          (current_seconds < 10 ? '0' : '') +
+          current_seconds;
+        if (seconds > 0) {
+          setTimeout(tick, 1000);
+        }
+        if (seconds <= 0) {
+          $('#andmedTimerModal').modal('show');
+        }
+      }
+      tick();
+    }
+    countdown(20);
 
   }
 
