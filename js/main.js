321549299a3c488fc0b5878b12724086700c7d37
reino7 <reino7@tlu.ee>
2019-11-24 01:00:54 +0200

    Üleliigse koodi ja failide eemaldamine / Uute failide linkimine / Gameboard UI parandamine

--- js/main.js
+++ js/main.js
@@ -1,117 +1,43 @@
 $(window).on('load', function() {
-
   // Get Hostname and port
   let host = location.host;
   let protocol = location.protocol;
-  
 
   // Player Role Card path
-  let linnaplaneerijaPath = "/rollid/linnaplaneerija"
-  let investorPath = "/rollid/investor/";
-  let sotsiaalnounikPath = "/rollid/sotsiaalnounik/";
-  let looduskaitsebioloogPath = "/rollid/looduskaitsebioloog/";
-  let kinnisvaraarendajaPath = "/rollid/kinnisvaraarendaja/";
-  let koolidirektorPath = "/rollid/koolidirektor/";
-  let aktiivneLinnaelanikPath = "/rollid/aktiivne_linnaelanik/";
+  let linnaplaneerijaPath = '/rollid/linnaplaneerija';
+  let investorPath = '/rollid/investor/';
+  let sotsiaalnounikPath = '/rollid/sotsiaalnounik/';
+  let looduskaitsebioloogPath = '/rollid/looduskaitsebioloog/';
+  let kinnisvaraarendajaPath = '/rollid/kinnisvaraarendaja/';
+  let koolidirektorPath = '/rollid/koolidirektor/';
+  let aktiivneLinnaelanikPath = '/rollid/aktiivne_linnaelanik/';
 
-
-  // Create Player Role URL base on
+  // Create Player Role URL based on
   // protocol, host and Player Role Card path
-  let linnaplaneerijaUrl = protocol + "//" + host + linnaplaneerijaPath;
-  let investorUrl = protocol + "//" + host + investorPath;
-  let sotsiaalnounikUrl = protocol + "//" + host + sotsiaalnounikPath;
-  let looduskaitsebioloogUrl = protocol + "//" + host + looduskaitsebioloogPath;
-  let kinnisvaraarendajaUrl = protocol + "//" + host + kinnisvaraarendajaPath;
-  let koolidirektorUrl = protocol + "//" + host + koolidirektorPath;
-  let aktiivneLinnaelanikUrl = protocol + "//" + host + aktiivneLinnaelanikPath;
-
+  let linnaplaneerijaUrl = protocol + '//' + host + linnaplaneerijaPath;
+  let investorUrl = protocol + '//' + host + investorPath;
+  let sotsiaalnounikUrl = protocol + '//' + host + sotsiaalnounikPath;
+  let looduskaitsebioloogUrl = protocol + '//' + host + looduskaitsebioloogPath;
+  let kinnisvaraarendajaUrl = protocol + '//' + host + kinnisvaraarendajaPath;
+  let koolidirektorUrl = protocol + '//' + host + koolidirektorPath;
+  let aktiivneLinnaelanikUrl = protocol + '//' + host + aktiivneLinnaelanikPath;
 
   // Get the filename of HTML files
   var fileName = window.location.pathname.substring(
     window.location.pathname.lastIndexOf('/') + 1
   );
 
-
-  // If in a specific HTML page, then run script
-  if (fileName == 'cityname_and_population.html') {
-
-    function countdown(seconds) {
-      seconds = parseInt(sessionStorage.getItem('seconds')) || seconds;
-      //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab.
-      //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
-      //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
-
-
-      function tick() {
-        seconds--;
-        //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi.
-        //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
-        sessionStorage.setItem('seconds', seconds);
-        let counter = document.getElementById('timer_1');
-        let current_minutes = parseInt(seconds / 60);
-        let current_seconds = seconds % 60;
-
-        // if (current_minutes < 3) {
-        //   //Bootstrap roheline värv
-        //   counter.style.color = '#28a745';
-        // }
-        // if (current_minutes < 2) {
-        //   //Bootstrap kollane/oranz värv
-        //   counter.style.color = '#ffc107';
-        // }
-        // if (current_minutes < 1) {
-        //   //Bootstrap punane värv
-        //   counter.style.color = '#dc3545';
-        // }
-
-        //Andmete välja näitamine html kujul
-        counter.innerHTML =
-          current_minutes +
-          ':' +
-          (current_seconds < 10 ? '0' : '') +
-          current_seconds;
-        if (seconds > 0) {
-          setTimeout(tick, 1000);
-        }
-        if (seconds <= 0) {
-          $('#andmedTimerModal').modal('show');
-        }
-      }
-      tick();
-    }
-    countdown(20);
-
-    
-    //Linnanime ja rahvaarvu salvestamine
-    document.getElementById("submit").addEventListener("click", saveData);
-    function saveData() {
-      let cityName = document.getElementById('cityName').value;
-      let population = document.getElementById('population').value;
-
-      sessionStorage.setItem('cityName', cityName);
-      sessionStorage.setItem('population', population);
-
-      return true;
-    }
-
-  }
-
-
-  // If in a specific HTML page, then run script
-  if (fileName == 'city_planner.html') {
-    document.getElementById('rollidLinnaplaneerijaID').innerHTML = linnaplaneerijaUrl;
-  }
-
-
   // If in a specific HTML page, then run script
   if (fileName == 'cityname_population_cityplanner.html') {
-    document.getElementById('rollidLinnaplaneerijaID').innerHTML = linnaplaneerijaUrl;
+    document.getElementById(
+      'rollidLinnaplaneerijaID'
+    ).innerHTML = linnaplaneerijaUrl;
     let urls = [linnaplaneerijaUrl];
 
     urls.forEach(makeQRCode);
 
     function makeQRCode(item, index) {
-      let qrcode = new QRCode(document.getElementById("qrcode_"+index), {
+      let qrcode = new QRCode(document.getElementById('qrcode_' + index), {
         text: item,
         width: 175,
         height: 175
@@ -163,6 +89,17 @@ $(window).on('load', function() {
     }
     countdown(20);
 
+    //Linnanime ja rahvaarvu salvestamine
+    document.getElementById('submit').addEventListener('click', saveData);
+    function saveData() {
+      let cityName = document.getElementById('cityName').value;
+      let population = document.getElementById('population').value;
+
+      sessionStorage.setItem('cityName', cityName);
+      sessionStorage.setItem('population', population);
+
+      return true;
+    }
   }
 
   // If in a specific HTML page, then run script
@@ -174,12 +111,19 @@ $(window).on('load', function() {
     document.getElementById('rollidKoolidirektorID').innerHTML = koolidirektorUrl;
     document.getElementById('rollidAktiivneLinnaelanikID').innerHTML = aktiivneLinnaelanikUrl;
 
-    let urls = [investorUrl, sotsiaalnounikUrl, looduskaitsebioloogUrl, kinnisvaraarendajaUrl, koolidirektorUrl, aktiivneLinnaelanikUrl];
+    let urls = [
+      investorUrl,
+      sotsiaalnounikUrl,
+      looduskaitsebioloogUrl,
+      kinnisvaraarendajaUrl,
+      koolidirektorUrl,
+      aktiivneLinnaelanikUrl
+    ];
 
     urls.forEach(makeQRCode);
 
     function makeQRCode(item, index) {
-      let qrcode = new QRCode(document.getElementById("qrcode_"+index), {
+      let qrcode = new QRCode(document.getElementById('qrcode_' + index), {
         text: item,
         width: 175,
         height: 175
@@ -202,134 +146,85 @@ $(window).on('load', function() {
     document.getElementsByTagName('Title')[0].text =
       cityNameShow + ' Elukeskkonna planeerimine';
 
-      function countdown(seconds) {
-        seconds = parseInt(sessionStorage.getItem('Timer 2')) || seconds;
-        //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab.
-        //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
-        //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
-  
-        function tick() {
-          seconds--;
-          //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi.
-          //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
-          sessionStorage.setItem('Timer 2', seconds);
-          let timer_2 = document.getElementById('timer_2');
-          let current_minutes = parseInt(seconds / 60);
-          let current_seconds = seconds % 60;
-  
-          // if (current_minutes < 3) {
-          //   //Bootstrap roheline värv
-          //   timer_2.style.color = '#28a745';
-          // }
-          // if (current_minutes < 2) {
-          //   //Bootstrap kollane/oranz värv
-          //   timer_2.style.color = '#ffc107';
-          // }
-          // if (current_minutes < 1) {
-          //   //Bootstrap punane värv
-          //   timer_2.style.color = '#dc3545';
-          // }
-  
-          //Andmete välja näitamine html kujul
-          timer_2.innerHTML =
-            current_minutes +
-            ':' +
-            (current_seconds < 10 ? '0' : '') +
-            current_seconds;
-          if (seconds > 0) {
-            setTimeout(tick, 1000);
-          }
-          if (seconds == 10) {
-            $('#halfTimeModal').modal('show');
-          }
-          if (seconds <= 0) {
-            $('#gameOverModal').modal('show');
-          }
+    function countdown(seconds) {
+      seconds = parseInt(sessionStorage.getItem('Timer 2')) || seconds;
+      //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab.
+      //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
+      //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
+
+      function tick() {
+        seconds--;
+        //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi.
+        //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
+        sessionStorage.setItem('Timer 2', seconds);
+        let timer_2 = document.getElementById('timer_2');
+        let current_minutes = parseInt(seconds / 60);
+        let current_seconds = seconds % 60;
+
+        // if (current_minutes < 3) {
+        //   //Bootstrap roheline värv
+        //   timer_2.style.color = '#28a745';
+        // }
+        // if (current_minutes < 2) {
+        //   //Bootstrap kollane/oranz värv
+        //   timer_2.style.color = '#ffc107';
+        // }
+        // if (current_minutes < 1) {
+        //   //Bootstrap punane värv
+        //   timer_2.style.color = '#dc3545';
+        // }
+
+        //Andmete välja näitamine html kujul
+        timer_2.innerHTML =
+          current_minutes +
+          ':' +
+          (current_seconds < 10 ? '0' : '') +
+          current_seconds;
+        if (seconds > 0) {
+          setTimeout(tick, 1000);
         }
-        tick();
+        if (seconds == 10) {
+          $('#halfTimeModal').modal('show');
+        }
+        if (seconds <= 0) {
+          $('#gameOverModal').modal('show');
+        }
       }
-      countdown(20);
+      tick();
+    }
+    countdown(20);
   }
 
+  // Activate fullscreen / Deactivate fullscreen
+  // Button needs to be a link !!!
+  if (document.fullscreenEnabled) {
+    var btn = document.getElementById('toggle');
 
-  // IS THIS CODE NEEDED?
+    btn.addEventListener(
+      'click',
+      function(event) {
+        if (!document.fullscreenElement) {
+          document.documentElement.requestFullscreen();
+        } else {
+          document.exitFullscreen();
+        }
+      },
+      false
+    );
 
-  // //Hetke asukoht URL'is.
-  // let x = location.href;
+    document.addEventListener('fullscreenchange', function(event) {
+      console.log(event);
 
-  // let cname_popUrl = location.protocol + location.hostname + location.port + "/cityname_and_population.html";
-  // let gameboardURL = location.protocol + location.hostname + location.port + "/gameboard.html";
+      if (!document.fullscreenElement) {
+        btn.innerText = 'Aktiveeri täisekraan';
+      } else {
+        btn.innerText = 'Deaktiveeri täisekraan';
+      }
+    });
 
-  //   //Hetkel andmete timer. Tuleb teha funktsioonides muudatusi kui tahame teha dünaamilisemaks teiste lehtede jaoks ka VB.
-  //   //kui urlid kattuvad, siis läheb kood tööle, cityname_and_population.html lehel.
-  //   if(cname_popUrl == x) {
-
-  //     function countdown(seconds) {
-  //       seconds = parseInt(sessionStorage.getItem("seconds")) || seconds;
-  //       //Kui on sekundeid mälus siis sessionStorage.getItem() need üles leiab.
-  //       //Kui pole, siis hakkab originaal 180 sekundit alla poole tiksuma.
-  //       //Kui timer on jooksnud 0:00, siis refresh alustab uuesti otsast peale.
-
-  //       function tick() {
-  //         seconds--;
-  //         //sessionStorage salvestab hetke aja mällu (alles olevad sekundid) võib teha refresh, minna edasi teistele lehtedele või tagasi.
-  //         //Veebileht tuleb kinni panna, et kaotada mälus olev seis.
-  //         sessionStorage.setItem("seconds", seconds)
-  //         let counter = document.getElementById("timer_1");
-  //         let current_minutes = parseInt(seconds/60);
-  //         let current_seconds = seconds % 60;
-
-  //         if(current_minutes < 3){
-  //           //Bootstrap roheline värv
-  //           counter.style.color = "#28a745";
-  //         }
-  //         if(current_minutes < 2){
-  //           //Bootstrap kollane/oranz värv
-  //           counter.style.color = "#ffc107";
-  //         }
-  //         if(current_minutes < 1){
-  //           //Bootstrap punane värv
-  //           counter.style.color = "#dc3545";
-  //         }
-
-  //         //Andmete välja näitamine html kujul
-  //         counter.innerHTML = current_minutes + ":" + (current_seconds < 10 ? "0" : "") + current_seconds;
-  //         if( seconds > 0 ) {
-  //           setTimeout(tick, 1000);
-  //         }
-  //         if(seconds <= 0){
-  //           $("#andmedTimerModal").modal("show");
-  //         }
-  //       }
-  //       tick();
-  //     }
-  //     countdown(180);
-
-  //     document.getElementById("submit").addEventListener("click", saveData);
-
-  //     //Linnanime ja rahvaarvu salvestamine
-  //     function saveData(){
-  //       let cityName = document.getElementById("cityName").value;
-  //       let population = document.getElementById("population").value;
-
-  //       sessionStorage.setItem("cityName", cityName);
-  //       sessionStorage.setItem("population", population);
-
-  //       return true;
-  //     }
-
-  //   }
-
-  //   //Kood läheb tööle gameboard.html lehel.
-  //   if(gameboardURL == x){
-
-  //     //Linnanime ja rahvaarvu kättesaamine.
-  //     let cityNameShow = sessionStorage.getItem("cityName");
-  //     let populationShow = sessionStorage.getItem("population");
-
-  //     document.getElementById("cityName").innerHTML = cityNameShow;
-  //     document.getElementById("population").innerHTML = populationShow;
-
-  //     document.getElementsByTagName("Title")[0].text = cityNameShow + " Elukeskkonna planeerimine";
-  //   }
+    document.addEventListener('fullscreenerror', function(event) {
+      console.log(event);
+    });
+  }
+  
 });
